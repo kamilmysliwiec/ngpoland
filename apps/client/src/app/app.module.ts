207fce7cf55f336d17ccd63c974cfaa652255bb5
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-11-13 15:18:42 +0100

    Initial commit

---
+++ apps/client/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NxModule } from '@nrwl/nx';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [AppComponent, DashboardComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
